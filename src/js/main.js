211b4ffc9c34dd7adabf16019272a13d74809443
yo16 <xxyo1xx@gmail.com>
2021-12-18 14:57:04 +0900

    数字を表示、グループ

--- src/js/main.js
+++ src/js/main.js
@@ -1,5 +1,10 @@
 // 初期
 $(function(){
+    // 設定
+    // 数を表示するかどうか
+    // 表示する場合はてきとうな幅に、しない場合は細い幅になる
+    var ShowNumber = true;
+
     // データを整形
     var data = {
         'nodes': [
@@ -10,22 +15,34 @@ $(function(){
             {'id': 'c2', 'title': 'Stage 2-2'}
         ],
         'links': [
-            {'source': 'a', 'target': 'b', 'type': 0, 'value': 2},
-            {'source': 'a', 'target': 'c', 'type': 1, 'value': 8},
-            {'source': 'c', 'target': 'c1', 'type': 1, 'value': 5},
-            {'source': 'c', 'target': 'c2', 'type': 2, 'value': 3},
+            {'source': 'a', 'target': 'b', 'type': 0, 'value': 2000},
+            {'source': 'a', 'target': 'c', 'type': 1, 'value': 8000},
+            {'source': 'c', 'target': 'c1', 'type': 1, 'value': 5200},
+            {'source': 'c', 'target': 'c2', 'type': 2, 'value': 2800},
+        ],
+        'groups': [
+            {'title': 'step2',
+             'nodes': ['b','c']},
+            {'title': 'step3',
+             'nodes': ['c1','c2']}
         ]
     };
 
     // ダイアグラムを定義
     var colors = d3.scaleOrdinal(d3.schemeCategory10);
     var diagram = d3.sankeyDiagram()
-                    .linkColor(function(d) { return colors(d.type); });
+                    .linkColor(function(d) { return colors(d.type); })
+                    .nodeValue(function(d) {
+                        return ShowNumber ? Number(d.value).toLocaleString() :'';
+                    })
+                    .groups(data.groups)
+    ;
 
     // レイアウトを定義
+    var nodeWidth = ShowNumber ? 60: 2;
     var layout = d3.sankey()
-            .nodeWidth(10)
-            .extent([[100, 0], [400, 200]]);
+            .nodeWidth(nodeWidth)
+            .extent([[100, 50], [400, 200]]);
 
     // 描画
     d3.select('#sankey_svg')
@@ -40,8 +57,6 @@ $(function(){
 function fitSvg(svg_id){
     var svg = $("#"+svg_id);
     var g = $("#"+svg_id+" g");
-    console.log(svg[0].getBoundingClientRect());
-    console.log(g[0].getBoundingClientRect());
     var g_rect = g[0].getBoundingClientRect()
     svg.width(g_rect.x + g_rect.width);
     svg.height(g_rect.y + g_rect.height);
